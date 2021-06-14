f55565c25c59e1b7047ce98025ee7e032ae1b73a
AhzekAhriman <soulreaver5@bk.ru>
2021-06-14 20:49:20 +0300

    MovementComponent without debugging.

--- Source/Motorsport/Components/LandraiderMovementComponent.cpp
+++ Source/Motorsport/Components/LandraiderMovementComponent.cpp
@@ -3,6 +3,10 @@
 
 #include "LandraiderMovementComponent.h"
 
+#include "Engine/World.h" // GetWorld
+#include "GameFramework/Actor.h" // Owner
+
+
 // Sets default values for this component's properties
 ULandraiderMovementComponent::ULandraiderMovementComponent()
 {
@@ -10,19 +14,228 @@ ULandraiderMovementComponent::ULandraiderMovementComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
+void ULandraiderMovementComponent::FullThrottle(float InputIntensity)
+{
+	// На случай ошибки вектора.
+	if (InputIntensity < 0.f)
+	{
+		InputIntensity *= -1.f;
+	}
+	// Если произошла смена направления движения, то сначала снижаем скорость до нуля, а потом уже разгоняемся.
+	if (CurrentSpeed < 0.f)
+	{
+		// Сбрасываем обратное ускорение.
+		if (SpeedChangeStep < 0.f)
+		{
+			SpeedChangeStep = 0.f;
+		}
+
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep + BrakingStep, 0.f, MaxAccelerationSpeed);
+	}
+	else
+	{
+		// Условное переключение между направлениями движения сопровождается сбросом "ускорения торможения" последующим набором "ускорения движения".
+		if (CurrentSpeed <= SpeedChangeStep && SpeedChangeStep != AccelerationStep)
+		{
+			SpeedChangeStep = 0.f;
+		}
+		
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep + AccelerationStep, 0.f, MaxAccelerationSpeed);
+	}
+	
+	CurrentSpeed = (CurrentSpeed + SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+	// Неизвестно с какой скорости начиналось, поэтому берём максимальный задний ход в качестве нижней границы.
+	// В качестве верхней - долю от максимальной, соответствующую интенсивности нажатия педальки.
+	FMath::Clamp(CurrentSpeed, MaxReverseSpeed, MaxMoveSpeed * InputIntensity);
+}
+
+void ULandraiderMovementComponent::ReverseGear(float InputIntensity)
+{
+	// Поддерживаем нужную скорость.
+	if (CurrentSpeed == MaxReverseSpeed * InputIntensity) return;
+	
+	// На случай ошибки вектора.
+	if (InputIntensity < 0.f)
+	{
+		InputIntensity *= -1.f;
+	}
+	// Если произошла смена направления движения, то сначала снижаем скорость до нуля, а потом уже разгоняемся назад.
+	if (CurrentSpeed > 0.f)
+	{
+		// Сбрасываем ускорение.
+		if (SpeedChangeStep > 0.f)
+		{
+			SpeedChangeStep = 0.f;
+		}
+		// Тормозим, пока не поменяется направление движения.
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep + BrakingStep, 0.f, MaxAccelerationSpeed);
+
+		CurrentSpeed = (CurrentSpeed - SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+	}
+	else
+	{
+		// Условное переключение между направлениями движения сопровождается сбросом "ускорения торможения" последующим набором "ускорения движения".
+		if (CurrentSpeed >= -SpeedChangeStep && SpeedChangeStep != -AccelerationStep)
+		{
+			SpeedChangeStep = 0.f;
+		}
+
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep - AccelerationStep, MaxAccelerationSpeed, 0.f);
+		CurrentSpeed = (CurrentSpeed + SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+	}
+
+	FMath::Clamp(CurrentSpeed, MaxReverseSpeed * InputIntensity, MaxMoveSpeed);
+}
+
+void ULandraiderMovementComponent::IdleMoving()
+{
+	// Если никуда не едем, то и хорошо.
+	if (CurrentSpeed == 0.f) return;
+
+	// Катимся назад.
+	if (CurrentSpeed < 0.f)
+	{
+		// По идее данная формулировка позволит на первом тике занулить отрицательное ускорение, а на следующем будет уже расти снижение скорости.
+		if (SpeedChangeStep < 0.f)
+		{
+			SpeedChangeStep = 0.f;
+		}
+		// "Повышаем" обратную скорость до нуля.
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep + IdleSpeedFadingStep, MaxAccelerationSpeed, 0.f);
+		CurrentSpeed = (CurrentSpeed + SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+		
+		FMath::Clamp(CurrentSpeed, MaxReverseSpeed, 0.f);
+	}
+	// Катимся вперёд.
+	if (CurrentSpeed > 0.f)
+	{
+		if (SpeedChangeStep > 0.f)
+		{
+			SpeedChangeStep = 0.f;
+		}
+		// "Понижаем" скорость до нуля.
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep + IdleSpeedFadingStep, 0.f, MaxAccelerationSpeed);
+		CurrentSpeed = (CurrentSpeed - SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+
+		FMath::Clamp(CurrentSpeed, 0.f, MaxMoveSpeed);
+	}
+}
+
+void ULandraiderMovementComponent::Braking()
+{
+	// Если никуда не едем, то и хорошо.
+	if (CurrentSpeed == 0.f) return;
+
+	// Тормозим заднюю скорость.
+	if (CurrentSpeed < 0.f)
+	{
+		// Сбрасываем ускорение задней скорости.
+		if (SpeedChangeStep < 0.f)
+		{
+			SpeedChangeStep = 0.f;
+		}
+		// Набираем "ускорение ториожения" и тормозим.
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep + BrakingStep, 0.f, MaxAccelerationSpeed);
+		CurrentSpeed = (CurrentSpeed + SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+		FMath::Clamp(CurrentSpeed, MaxReverseSpeed, 0.f);
+	}
+	// Тормозим переднюю скорость.
+	if(CurrentSpeed > 0.f)
+	{
+		// Сбрасываем ускорение движения.
+		if (SpeedChangeStep > 0.f)
+		{
+			SpeedChangeStep = 0.f;
+		}
+		// Набираем "ускорение ториожения" и тормозим.
+		SpeedChangeStep = FMath::Clamp(SpeedChangeStep - BrakingStep, -MaxAccelerationSpeed, 0.f);
+		CurrentSpeed = (CurrentSpeed + SpeedChangeStep) * GetWorld()->GetDeltaSeconds();
+		FMath::Clamp(CurrentSpeed, MaxReverseSpeed, 0.f);
+	}
+}
+
+
+
+void ULandraiderMovementComponent::MoveForward()
+{
+	GetOwner()->AddActorLocalOffset(FVector(CurrentSpeed, 0.f, 0.f), true);
+}
+
+void ULandraiderMovementComponent::CalculateTurnRate(float TurnRateInput)
+{
+	// Чем выше скорость - тем сложнее повернуть.
+	
+	
+	//CurrentTurnRate = MaxTurnSpeed * 
+}
+
+void ULandraiderMovementComponent::Turn()
+{
+	GetOwner()->AddActorLocalRotation(CurrentTurn, true);
+}
+
+void ULandraiderMovementComponent::Skidding()
+{
+
+}
 
 // Called when the game starts
 void ULandraiderMovementComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
+float ULandraiderMovementComponent::SetDriveAction(EDriveAction Action, float InputIntensity)
+{
+	// Конечная скорость движения.
+	float ThrottleInput = FMath::Clamp(InputIntensity, -1.f, 1.f);
+	CurrentDriveAction = Action;
+
+	switch (CurrentDriveAction)
+	{
+	case EDriveAction::FullThrottle:
+		FullThrottle(ThrottleInput);
+		break;
+	
+	case EDriveAction::ReverseGear:
+		ReverseGear(ThrottleInput);
+		break;
+	
+	case EDriveAction::IdleMoveing:
+		IdleMoving();
+		break;
+	
+	case EDriveAction::Braking:
+		Braking();
+		break;
+	
+	default:
+		break;
+	}
+
+	return CurrentSpeed;
+}
+
+FQuat ULandraiderMovementComponent::Steering(float TurnRateInput)
+{
+	//float RotateAmount = Value * RotateSpeed * GetWorld()->DeltaTimeSeconds;
+	//FRotator Rotation = FRotator(0, RotateAmount, 0);
+	//RotationDirection = FQuat(Rotation);
+	if (CurrentSpeed != 0.f)
+	{
+		CurrentTurnRate = MaxTurnRate * FMath::Clamp(1.f - (abs(CurrentSpeed) / MaxMoveSpeed), 0.1f, 1.f);
+		FRotator Rotation = FRotator(0, CurrentTurnRate, 0);
+		CurrentTurn = FQuat(Rotation);
+	}
+	else
+	{
+		CurrentTurn = FQuat(FVector(0.f, 0.f, 0.f), 0.f);
+	}
+
+	return CurrentTurn;
+}
 
 // Called every frame
 void ULandraiderMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
--- Source/Motorsport/Components/LandraiderMovementComponent.h
+++ Source/Motorsport/Components/LandraiderMovementComponent.h
@@ -7,6 +7,15 @@
 #include "LandraiderMovementComponent.generated.h"
 
 
+UENUM()
+enum class EDriveAction : uint8
+{
+	FullThrottle,
+	ReverseGear,
+	IdleMoveing,
+	Braking
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MOTORSPORT_API ULandraiderMovementComponent : public UActorComponent
 {
@@ -16,11 +25,83 @@ public:
 	// Sets default values for this component's properties
 	ULandraiderMovementComponent();
 
+private:
+
+//	FVector MoveDirection;
+	FQuat CurrentTurn;
+	//APawn* Landraider = nullptr;
+
+	EDriveAction CurrentDriveAction = EDriveAction::IdleMoveing;
+
+	float CurrentSpeed = 0.f;
+	float CurrentTurnSpeed = 0.f;
+	float SpeedChangeStep = 0.f;
+	float CurrentTurnRate = 0.f;
+
+	bool bFullThrottle = false;
+	bool bIsBraking = false;
+	
+//	void CalculateRotateInput(float Value);
+
+
+	// Move Behaviour
+	void FullThrottle(float InputIntensity);
+	void ReverseGear(float InputIntensity);
+	void IdleMoving();
+	void Braking();
+
+	void MoveForward();
+	
+	// Reering Behaviour
+	void CalculateTurnRate(float TurnRateInput);
+	void Turn();
+
+	// TODO: Реализовать занос. Если останется время.
+	void Skidding();
+
 protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MaxMoveSpeed = 200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MaxReverseSpeed = -100.0f;
+	
+	// Максимальная скорость поворота, она же угол.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MaxTurnRate = 100.0f;
+
+	// Время разгона
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float AccelerationStep = 0.02f;
+	
+	// Максимальное ускорение
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float MaxAccelerationSpeed = 2.f;
+
+
+	//Время остановки
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float IdleSpeedFadingStep = 0.01f;
+
+	//Время торможения
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float BrakingStep = 0.04f;
+
+	
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
+	
+	// Газ-Тормоз
+	UFUNCTION(BlueprintCallable)
+	float SetDriveAction(EDriveAction Action, float InputIntensity);
+		
+	// Поворот
+	UFUNCTION(BlueprintCallable)
+	FQuat Steering(float TurnRateInput);
+
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
